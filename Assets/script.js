8aa7232acaa035863846c78bba9a81a3b9e59556
Kwis <random@gcom>
2020-12-10 10:25:30 +0800

    accidentally removed a event with function

--- Assets/script.js
+++ Assets/script.js
@@ -122,7 +122,7 @@ $(document).ready(function() {
       $plannerDiv.append($rowDiv);
     };
 
-  
+    
     // function to update row color
     function updateRowColor ($hourRow,hour) { 
   
@@ -170,6 +170,13 @@ $(document).ready(function() {
       localStorage.setItem("storedPlans", JSON.stringify(planTextArr));
     });  
     
+    // function to color save button on change of input
+    $(document).on('change','input', function(event) {
+      event.preventDefault();  
+      if (test) { console.log('onChange'); }
+      if (test) { console.log('id', $(this).attr('hour-index')); }
+
+     
   
       let i = $(this).attr('hour-index');
   
